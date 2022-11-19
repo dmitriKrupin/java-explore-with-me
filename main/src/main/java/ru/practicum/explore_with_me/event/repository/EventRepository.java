ca25ce07af4361d655da6252c571dbc7f69bc2fe
Dmitriy Krupin <dmitri.krupin@yandex.ru>
2022-11-19 16:41:22 +0300

    EventRepository has been updated.

--- main/src/main/java/ru/practicum/explore_with_me/event/repository/EventRepository.java
+++ main/src/main/java/ru/practicum/explore_with_me/event/repository/EventRepository.java
@@ -14,20 +14,10 @@ public interface EventRepository extends JpaRepository<Event, Long> {
 
     List<Event> findAllByIdIn(List<Long> events);
 
-    //String text, String categories, Boolean paid, String rangeStart,
-    //String rangeEnd, Boolean onlyAvailable, String sort, Long from, Long size
-    /*@Query(" select i from Item i " +
-            "where upper(i.name) like upper(concat('%', ?1, '%')) " +
-            " or upper(i.description) like upper(concat('%', ?1, '%'))" +
-            " and i.available = true ")*/
-    //List<Item> findItemListBySearch(String text);
-
     List<Event> findAllByAnnotationLikeAndCategoryIdAndPaid(
             String annotation, Long category_id, Boolean paid);
 
-    //List<Long> users, List<String> states, List<Long> categories,
-    //String rangeStart, String rangeEnd, Long from, Long size
     List<Event> findAllByInitiator_IdInAndStateInAndCategoryIdInAndEventDateAfterAndEventDateBefore(
-            Collection<Long> initiator_id, Collection<Status> state,
-            Collection<Long> category_id, LocalDateTime rangeStart, LocalDateTime rangeEnd);
+            Collection<Long> users, Collection<Status> states,
+            Collection<Long> categories, LocalDateTime rangeStart, LocalDateTime rangeEnd);
 }
